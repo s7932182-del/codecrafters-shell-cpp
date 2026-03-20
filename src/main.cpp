d25521f7e33473358e2189aaf6a7d3cf51704d4f
ani <ironmantoni131@gmail.com>
2026-03-20 19:16:20 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <string>
 
-int main(char * argv[]) {
+int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
+
+  // Read the user Input
+
+  std::string input;
+   
+  std::cin >> input;
+
+  std::cerr << input << ": command not found" << std::endl;
+
+  
+
 }
 
