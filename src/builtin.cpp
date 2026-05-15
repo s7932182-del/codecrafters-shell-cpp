31553b5fae425cb91097fd60fad7365df5101614
ani <ironmantoni131@gmail.com>
2026-05-15 07:36:20 +0530

    codecrafters submit [skip ci]

--- src/builtin.cpp
+++ src/builtin.cpp
@@ -278,25 +278,30 @@ void COMPLETE::execute(const std::vector<std::string> &args) {
 
 
     for (auto it = args.begin() + 1; it != args.end(); ++it) {
-           const auto &arg = *it;
+        const auto &arg = *it;
 
-           if (arg == "-p") {
-               ++it;
-               const std::string &cmd = *it;
-               const auto &script_it = script_list.find(cmd);
+        if (arg == "-p") {
+            ++it;
+            const std::string &cmd = *it;
+            const auto &script_it = script_list.find(cmd);
 
-               if (script_it != script_list.end()) {
-                   std::cout << "complete -C " << std::quoted(script_it->second, '\'') << " " << cmd << std::endl;
-               } else {
-                   std::cout << "complete: " << cmd << ": " << "no completion specification" << std::endl;
-               }
-           } else if (arg == "-C") {
-               ++it;
-               const std::string &path = *it;
-               ++it;
-               const std::string &cmd = *it;
-               script_list[cmd] = path;
-           }
+            if (script_it != script_list.end()) {
+                std::cout << "complete -C " << std::quoted(script_it->second, '\'') << " " << cmd << std::endl;
+            } else {
+                std::cout << "complete: " << cmd << ": " << "no completion specification" << std::endl;
+            }
+        }
+        else if (arg == "-C") {
+            ++it;
+            const std::string &path = *it;
+            ++it;
+            const std::string &cmd = *it;
+            script_list[cmd] = path;
+        } else if (arg == "-r") {
+            ++it;
+            const std::string cmd = *it;
+            it = script_list.erase(cmd);
+        }
     }
 
 }
